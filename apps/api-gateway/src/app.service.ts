52bbcdfec804675b3878b7f17203f840ece42ced
Caua Mattos <caua.h4k@gmail.com>
2025-12-07 18:46:11 -0300

    refactor(api-gateway): remove unecessary imports and lines

--- apps/api-gateway/src/app.service.ts
+++ apps/api-gateway/src/app.service.ts
@@ -1,11 +1,7 @@
-import { ConflictException, Injectable } from '@nestjs/common';
-import { JwtService } from '@nestjs/jwt';
-import { UserDTO } from '../../../packages/types/dto/user/user.dto';
+import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  constructor(private readonly jwtService: JwtService) {}
-
   getHello(): string {
     return 'Hello World!';
   }
