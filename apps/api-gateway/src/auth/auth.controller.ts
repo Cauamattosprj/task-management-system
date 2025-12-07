be9fac50e9334428e01d7a609c4ec960831e36b4
Caua Mattos <caua.h4k@gmail.com>
2025-12-07 18:45:41 -0300

    feat(api-gateway): add register http endpoint and use dto aliases for import

--- apps/api-gateway/src/auth/auth.controller.ts
+++ apps/api-gateway/src/auth/auth.controller.ts
@@ -1,13 +1,20 @@
 import { Body, Controller, Inject, Post } from '@nestjs/common';
-import { UserLoginDTO } from '../../../../packages/types/dto/user/login.dto';
+import { UserLoginDTO } from '@dtos/login.dto';
+import { UserRegisterDTO } from '@dtos/register.dto';
 import { AUTH_SERVICE } from 'src/app.module';
 import { ClientProxy } from '@nestjs/microservices';
 
 @Controller('auth')
 export class AuthController {
   constructor(@Inject(AUTH_SERVICE) private readonly authClient: ClientProxy) {}
+
   @Post('login')
   login(@Body() userLoginDto: UserLoginDTO) {
     return this.authClient.send('auth-login', userLoginDto);
+  }
+
+  @Post('register')
+  register(@Body() userRegisterDto: UserRegisterDTO) {
+    return this.authClient.send('auth-register', userRegisterDto);
   }
 }
