dc29fbbe30f8eb9741cc3d20f3a0d545efd45b50
Caua Mattos <caua.h4k@gmail.com>
2025-12-07 17:16:02 -0300

    feat(api-gateway): remove unecessary logic from service

--- apps/api-gateway/src/app.service.ts
+++ apps/api-gateway/src/app.service.ts
@@ -6,24 +6,6 @@ import { UserDTO } from '../../../packages/types/dto/user/user.dto';
 export class AppService {
   constructor(private readonly jwtService: JwtService) {}
 
-  async registerUser(userDTO: UserDTO) {
-    existingUser = this.userRepository.findByEmail(userDTO.email);
-    if (existingUser) {
-      throw new ConflictException('User already registered');
-    }
-
-    registeredUser = await this.userRepository.save(userDTO);
-
-    const token = this.jwtService.sign({
-      sub: this.registerUser.id,
-      login: this.registerUser.email,
-    });
-
-    return {
-      user: this.registerUser,
-      token,
-    };
-  }
   getHello(): string {
     return 'Hello World!';
   }
