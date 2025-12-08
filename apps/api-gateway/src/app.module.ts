c4f66b8c6f41d690f330b38ad9e5f17ad8b996a5
Caua Mattos <caua.h4k@gmail.com>
2025-12-08 13:41:56 -0300

    refactor(api-gateway): add env variables

--- apps/api-gateway/src/app.module.ts
+++ apps/api-gateway/src/app.module.ts
@@ -5,16 +5,24 @@ import { ClientsModule, Transport } from '@nestjs/microservices';
 import { AuthController } from './auth/auth.controller';
 import { AUTH_SERVICE } from './constants';
 import { UserController } from './user/user.controller';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
     ClientsModule.register([
       {
         name: AUTH_SERVICE,
         transport: Transport.RMQ,
         options: {
-          urls: ['amqp://admin:admin@localhost:5672'],
-          queue: 'auth_queue',
+          urls: [
+            (process.env.RABBITMQ_URL as string) ||
+              'amqp://admin:admin@localhost:5672',
+          ],
+          queue: (process.env.AUTH_QUEUE as string) || 'auth_queue',
           queueOptions: {
             durable: true,
           },
