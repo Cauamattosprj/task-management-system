a48acc652154c7a69d486d8cacc9b54f5c271f2e
Caua Mattos <caua.h4k@gmail.com>
2025-12-09 14:52:26 -0300

    feat(user-service): add new user-service microservice

---
+++ apps/users-service/src/main.ts
@@ -0,0 +1,25 @@
+import { NestFactory } from '@nestjs/core';
+import { UserModule } from './user.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { Logger } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    UserModule,
+    {
+      transport: Transport.RMQ,
+      options: {
+        urls: [process.env.RABBITMQ_URL as string],
+        queue: process.env.USER_QUEUE as string,
+        queueOptions: {
+          durable: true,
+        },
+      },
+    },
+  );
+  await app.listen();
+}
+
+Logger.log('User Service is running');
+
+bootstrap();
---
+++ apps/users-service/src/user.module.ts
@@ -0,0 +1,30 @@
+import { Module } from '@nestjs/common';
+import { UserController } from './user.controller';
+import { UserService } from './user.service';
+import { User } from './user.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forFeature([User]),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST || 'localhost',
+      port: Number(process.env.POSTGRES_PORT) || 5432,
+      username: process.env.POSTGRES_USER || 'postgres',
+      password: process.env.POSTGRES_PASSWORD || 'password',
+      database: process.env.POSTGRES_DB || 'auth_service',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      migrations: [__dirname + '/migrations/*.ts'],
+      synchronize: process.env.NODE_ENV !== 'production',
+      logging: true,
+    }),
+  ],
+  controllers: [UserController],
+  providers: [UserService],
+})
+export class UserModule {}
