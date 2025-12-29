6d3a429047d3dc6efa020bf6c28bc41314666f7f
Caua Mattos <caua.h4k@gmail.com>
2025-12-29 11:28:41 -0300

    fix(platform): redirect logic in authentication check

--- apps/web/src/routes/_protected/platform.tsx
+++ apps/web/src/routes/_protected/platform.tsx
@@ -5,10 +5,9 @@ import { createFileRoute, Outlet, redirect } from "@tanstack/react-router";
 export const Route = createFileRoute("/_protected/platform")({
   beforeLoad: ({ context }) => {
     const { isAuthenticated } = context.auth;
+
     if (!isAuthenticated) {
-      throw redirect({
-        to: "/login",
-      });
+      throw redirect({ to: "/login" });
     }
   },
   component: RouteComponent,
