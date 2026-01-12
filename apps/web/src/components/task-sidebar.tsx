b83a80348d441d7884976bf9c9d1568aeafa2f9d
Caua Mattos <caua.h4k@gmail.com>
2026-01-12 20:36:26 -0300

    feat(ui): enhance task sidebar with status, priority, and deadline components

--- apps/web/src/components/task-sidebar.tsx
+++ apps/web/src/components/task-sidebar.tsx
@@ -1,18 +1,32 @@
+import { useState } from "react";
+import { AssignUsersCombobox } from "./assign-users-combobox";
+import { TaskDeadlinePicker } from "./task-deadline-picker";
 import { Button } from "./ui/button";
+import { TaskStatusCombobox } from "./task-status-combobox";
+import { TaskPriorityCombobox } from "./task-priority-combobox";
 
 export function TaskSidebar() {
+  const [assignedUsers, setAssignedUsers] = useState();
+  function handleDeadlineChange(): (date: Date | undefined) => void {
+    throw new Error("Function not implemented.");
+  }
+
+  function handleStatusChange(): (status) => void {
+    throw new Error("Function not implemented.");
+  }
+
+  function handlePriorityChange(): (priority) => void {
+    throw new Error("Function not implemented.");
+  }
+
   return (
     <div className="w-1/3 h-full border-l p-4">
       <div className="grid grid-cols-[120px_1fr] gap-y-2 items-center">
         <span className="text-muted-foreground">Status</span>
-        <Button variant="ghost" className="w-full justify-start">
-          Exemplo
-        </Button>
+        <TaskStatusCombobox onChange={handleStatusChange} />
 
         <span className="text-muted-foreground">Priority</span>
-        <Button variant="ghost" className="w-full justify-start">
-          Exemplo
-        </Button>
+        <TaskPriorityCombobox onChange={handlePriorityChange} />
 
         <span className="text-muted-foreground">Created By</span>
         <Button variant="ghost" className="w-full justify-start">
@@ -20,14 +34,10 @@ export function TaskSidebar() {
         </Button>
 
         <span className="text-muted-foreground">Assignees</span>
-        <Button variant="ghost" className="w-full justify-start">
-          Exemplo
-        </Button>
+        <AssignUsersCombobox setAssignedUsers={setAssignedUsers} />
 
         <span className="text-muted-foreground">Deadline</span>
-        <Button variant="ghost" className="w-full justify-start">
-          Exemplo
-        </Button>
+        <TaskDeadlinePicker onChange={handleDeadlineChange} />
       </div>
     </div>
   );
